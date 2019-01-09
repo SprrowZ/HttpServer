16ec57825234b631625d2bfd19eb4ec95fc0fdab
SprrowZ <33713300+SprrowZ@users.noreply.github.com>
2019-01-09 20:02:19 +0800

    提交

--- src/com/zzg/actions/UserAction.java
+++ src/com/zzg/actions/UserAction.java
@@ -2,7 +2,7 @@ package com.zzg.actions;
 
  
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+ 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,15 +10,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-import javax.imageio.IIOException;
+ 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.logging.log4j.core.appender.rolling.SizeBasedTriggeringPolicy;
+import org.apache.commons.io.FileUtils;
+ 
 import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
-import com.sun.org.apache.bcel.internal.generic.NEW;
+ 
  
 
 public class UserAction extends ActionSupport{
@@ -29,6 +30,11 @@ public class UserAction extends ActionSupport{
 	private static final long serialVersionUID = 1L;
 	private String userName;
 	private String password;
+	//上传文件,mPhoto跟客户端保持一致，这样就可以直接接收，
+	//跟上面的userName和password一样，只不过不是私有的，改成私有的就得加getter、setter
+	public File mPhoto;
+	//这个写法也是固定的，FileName，不能改变大小写
+	public String mPhotoFileName;
 	/**
 	 * 模仿用户登录
 	 * @return
@@ -78,17 +84,19 @@ public class UserAction extends ActionSupport{
 	 HttpServletRequest request=ServletActionContext.getRequest();
 	 InputStream inputStream=request.getInputStream();
 	 //File
-	 String fileName="D:/okhttpServer/zzg.jpg";
-	 
-	 File file = new  File(fileName);
-	 if (!file.exists()) {
-	   //  File dirs=new File(fileName.substring(0,fileName.lastIndexOf("/")));
-		 File dirs=file.getParentFile();
-		if (!dirs.isDirectory()||!dirs.exists()) {
-			dirs.mkdirs();
-		}
-		file.createNewFile();
-	}
+//	 String fileName="D:/okhttpServer/zzg.jpg";
+//	 
+//	 File file = new  File(fileName);
+//	 if (!file.exists()) {
+//	   //  File dirs=new File(fileName.substring(0,fileName.lastIndexOf("/")));
+//		 File dirs=file.getParentFile();
+//		if (!dirs.isDirectory()||!dirs.exists()) {
+//			dirs.mkdirs();
+//		}
+//		file.createNewFile();
+//	}
+	 String dir=ServletActionContext.getServletContext().getRealPath("files");
+	 File file=new File(dir,"zzzz.jpg");
 	 FileOutputStream fos = new FileOutputStream(file);
 	 int len=0;
 	 byte[] buf=new byte[1024*2];
@@ -99,6 +107,32 @@ public class UserAction extends ActionSupport{
 	 fos.flush();
 	 fos.close();
 	}
+	/**
+	 * 上传文件和其他信息
+	 * @return
+	 * @throws IOException
+	 */
+	public String uploadInfo() throws IOException{
+		if (mPhoto==null) {
+			System.out.println(mPhotoFileName+"is null!");
+		}
+		String dir="D:/okhttpServer/"+mPhotoFileName;
+		File file=new File(dir);
+		 if (!file.exists()) {
+			   //  File dirs=new File(fileName.substring(0,fileName.lastIndexOf("/")));
+				 File dirs=file.getParentFile();
+				if (!dirs.isDirectory()||!dirs.exists()) {
+					dirs.mkdirs();
+				}
+				file.createNewFile();
+			}
+		 
+		FileUtils.copyFile(mPhoto, file);
+		System.out.println("fileName"+mPhotoFileName+"||userName:"+userName
+				+"||password:"+password);
+		return null;
+	}
+	
 	public String getUserName() {
 		return userName;
 	}
